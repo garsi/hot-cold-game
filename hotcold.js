7bb0faf14353282be2f4bb1b4f79fe40b0b8779d
garsi <katarinagarsi@gmail.com>
2013-10-25 21:48:46 -0700

    Complete function and style options

--- hotcold.js
+++ hotcold.js
@@ -4,83 +4,67 @@ $(document).ready(function() {
 	var answer = Math.floor((Math.random()*100)+1); 
 	console.log(answer);
 
+
 	//Ask player for their guess
-	//var guess = prompt("Pick a number between 1 and 100" + "");
-	//console.log(guess);
-	function guessnumber() {
-		var guess = document.forms['HotCold'].guessbox.value;
-		console.log(guess);
+	var guess = prompt("Pick a number between 1 and 100" + "");
+		console.log(guess + " is the original guess")
 
-	//Validate player guess as proper value
 
+	//???Validate player guess as proper value - needs to be every time submission is entered
+	if(guess == NaN || guess == null || guess < 1 || guess > 100) {	
+    	var guess = prompt("Try that again. Please enter a number between 1 and 100" + "");
+	  } else {
+	  	console.log("Guess submission is valid");
+	  }
 
 
 	//Check if player guess it too high or too low
-		if(guess == answer) {
-			console.log("You win!");
-		} else if (guess < answer) {
+	function guessinitial() {
+		if(guess == answer){
+			confirm("Hey hot stuff, you win!");
+		} else if(guess < answer) {
 			var newGuess = prompt("too low, try again" + "");
 		} else {
 			var newGuess = prompt("too high, try again" + "");
 		}
-		console.log(newGuess);
-
-});
-
-
-$('#yourid').val();
-
-//Check difference of original and new guesses to the answer
-	function differenceOriginal(answer, newGuess) { 
-		return Math.abs(answer - newGuess);
-	}
-	console.log(differenceOriginal);
-
-	function differenceNew(answer, guess) { 
-		return Math.abs(answer - guess);
-	}
-	console.log(differenceNew);
-
-	
-	//Compare difference of guesses
-
-
-
-	//Continue guessing loop until answer is acheived
-	//newGuess = guess;
-
-
-
-});
-
-/*
-
-		if(guess == answer) {
-				alert("you win!");
-			} else if(guess < answer){
-				prompt("too low, try again" + "");
-			} else {
-				prompt("too high, try again" + "");
+		console.log(newGuess + " is the new guess");
+		window.newGuess = newGuess;
 		}
-	}
 
-
-	while(guess!==answer) {
-		if(guess < answer) {
-			var guessLow = prompt("too high, try again" + "");
-			guessLow = guess;
-			console.log(guess);
-		}else {
-			var guessHigh = prompt("too low, try again" + "");
-			guessHigh = guess;
-			console.log(guess);
-		}}
+	guessinitial();
 	
-	if(guess == answer) {
-		prompt("you win!");
+
+	//Check distance of original and new guesses to the answer
+	var distanceOrigin = Math.abs(guess-answer);
+		console.log(distanceOrigin + " is original guess distance");
+
+	var distanceNew = Math.abs(newGuess-answer);
+		console.log(distanceNew + " is new guess distance");
+
+
+	//Compare difference of guesses
+	while(distanceNew !== 0) {
+		function guesses() {
+			if(distanceNew <= distanceOrigin) {
+				var newGuess = prompt("Getting warmer, try again" + "");
+			} else {
+				var newGuess = prompt("Getting colder, try again" + "");
+			}
+
+			console.log(newGuess + " is the new guess");
+			window.newGuess = newGuess;
+		}
+
+		guesses();
+
+		var distanceOrigin = distanceNew;
+			console.log(distanceOrigin + " is now previous guess distance");
+		
+		var distanceNew = Math.abs(newGuess-answer);
+			console.log(distanceNew + " is newest guest distance");
+
 	}
+
+	confirm("you win!");
 
 });
-*/
-
-
